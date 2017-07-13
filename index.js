a54d177f54a59eca8052617040b0a2a3d26c1671
Ingo Renner <ingo@august.com>
2017-07-12 22:01:55 -0700

    Add entry lib point

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// TODO add LICENSE file
+// TODO add eslint
+// TODO add scripts folder - GitHub script to rule them all
+
+
+
+
+exports = module.exports = require('./lib/ping');
+
+exports.checks = require('./lib/connectionschecks');
