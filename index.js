235a8598cb00961eee60168c5b73d286a37c78e7
Ingo Renner <ingo@august.com>
2017-08-15 18:05:06 -0700

    Move ToDos to separate file

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-// TODO add eslint
-// TODO add scripts folder - GitHub script to rule them all
-
-
-
-
 exports = module.exports = require('./lib/health');
 
 exports.checks = require('./lib/connectionschecks');
