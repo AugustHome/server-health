46cced07dd1831fe7f177ff980a2452e3bb5074f
Ingo Renner <ingo@august.com>
2017-10-10 09:59:28 -0700

    Add test helper function to execute health requests

--- test/health.js
+++ test/health.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Promise = require('bluebird');
 const assert = require('chai').assert;
 const sinon = require('sinon');
 const restify = require('restify');
@@ -9,6 +10,53 @@ const serverHealth = require('../lib/health');
 
 
 describe('server health', function () {
+
+  /**
+   * Helper function to run requests against the health endpoint
+   *
+   * @return {Promise.<Object>} Resolves over the server response with parsed body
+   */
+  function getHealth() {
+    return new Promise(function (resolve, reject) {
+      http.get({
+        host: 'localhost',
+        port: 8080,
+        path: '/health'
+      }, (response) => {
+        const contentType = response.headers['content-type'];
+        let error;
+        let rawData = '';
+
+        if (response.statusCode !== 200) {
+          error = new Error(`Request Failed. Status Code: ${response.statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+          error = new Error('Invalid content-type.\n' +
+            `Expected application/json but received ${contentType}`);
+        }
+
+        if (error) {
+          reject(error.message);
+          response.resume();
+          return;
+        }
+
+        response.setEncoding('utf8');
+        response.on('data', (chunk) => { rawData += chunk; });
+        response.on('end', () => {
+          try {
+            response.body = JSON.parse(rawData);
+            return resolve(response);
+          } catch (err) {
+            reject(err);
+          }
+        });
+
+      }).on('error', function (err) {
+        reject(err);
+      });
+    });
+  }
+
 
   describe('exposeHealthEndpoint', () => {
 
@@ -44,17 +92,13 @@ describe('server health', function () {
       server.close();
     });
 
-    it('calls all connection checks', function (done) {
-      http.get({
-        host: 'localhost',
-        port: 8080,
-        path: '/health'
-      }, function (response) {
+    it('calls all connection checks', () => {
+      return getHealth()
+      .then((response) => {
         assert.equal(response.statusCode, 200);
 
         assert.isTrue(checkStubOne.called);
         assert.isTrue(checkStubTwo.called);
-        done();
       });
     });
 
