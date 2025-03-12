1d17791946762f689626892f42b913313fbc8497
Ingo Renner <ingo.renner@gmail.com>
2025-03-11 20:11:31 -0700

    Add text-summary reporter to nyc coverage command

--- scripts/test.sh
+++ scripts/test.sh
@@ -64,7 +64,7 @@ fi
 
 # create coverage report?
 if [ ${USE_COVERAGE} ]; then
-  MOCHA="$PWD/node_modules/.bin/nyc --reporter=html --reporter=text --reporter=lcov ${MOCHA_BIN}"
+  MOCHA="$PWD/node_modules/.bin/nyc --reporter=html --reporter=text --reporter=text-summary --reporter=lcov ${MOCHA_BIN}"
 fi
 
 # environment information
